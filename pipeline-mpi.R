e55fdaa8c396616b19ccdeaba4b3889b5c6ec045
Villegas-Diaz <Roberto.VillegasDiaz@sdstate.edu>
2019-09-09 14:40:21 -0500

    Non-parametric QTL Analysis added

--- pipeline-mpi.R
+++ pipeline-mpi.R
@@ -279,8 +279,16 @@ set.seed(SEED)
 x.normal <- jittermap(x.normal)
 x.normal <- calc.genoprob(x.normal, step=1, error.prob=0.001)
 individuals.phenotyped <- summary(x.normal)[[2]]
+
+x.non.parametric <- read.cross("csvs",".",
+                               paste0(output.files.prefix,".non.parametric.gen.csv"),
+                               paste0(output.files.prefix,".non.parametric.phe.csv"))
+x.non.parametric <- jittermap(x.non.parametric)
+x.non.parametric <- calc.genoprob(x.non.parametric, step=1, error.prob=0.001)
+
 print("Starting with QTL Analysis")
 print("Starting with Normal QTL Analysis")
+individuals.phenotyped.np <- summary(x.non.parametric)[[2]]
 
 # In case R exits unexpectedly, have it automatically clean up
 # resources taken up by Rmpi (slaves, memory, etc...)
@@ -497,179 +505,137 @@ x.non.parametric.scanone <- foreach(i=2:ncol(x.non.parametric$pheno),
                                     }
 
 
-stopCluster(cl) # Stop cluster
+x.non.parametric.summary.mapping <- foreach(i=2:ncol(x.non.parametric$pheno),
+                                            .combine = rbind,
+                                            .packages = c("ggplot2","grid","gridExtra","latex2exp","qtl","R.devices")) %dopar% {
+                                              #transformation.info <- non.parametric.transformed.meansp$feature == features.np[i]
+                                              #transformation.info <- non.parametric.transformed.meansp[transformation.info,c("transf","transf.value")][1,]
+                                              
+                                              record <- data.frame(
+                                                ID = i - 1,
+                                                qtl.ID = NA,
+                                                trait = features.np[i],
+                                                ind = individuals.phenotyped.np,
+                                                lg = NA,
+                                                lod.peak = NA,
+                                                pos.peak = NA,
+                                                marker = NA,
+                                                pos.p95.bay.int = NA,
+                                                marker.p95.bay.int = NA,
+                                                #pvar = NA,
+                                                #est.add = NA,
+                                                #est.dom = NA,
+                                                p5.lod.thr = NA,
+                                                p10.lod.thr = NA,
+                                                #p.val = NA,
+                                                #transf = transformation.info$transf,
+                                                #transf.val = transformation.info$transf.value,
+                                                method = "non.parametric-scanone",
+                                                p5.qtl = FALSE,
+                                                p10.qtl = FALSE
+                                              )
+                                              
+                                              is.pseudo.marker <- function(marker){
+                                                if(grepl("loc",marker)){
+                                                  return(TRUE)
+                                                }
+                                                return(FALSE)
+                                              }
+                                              
+                                              transform.pseudomarker <- function(cross, marker, chr, pos){
+                                                new.marker <- marker
+                                                new.pos <- pos
+                                                if(is.pseudo.marker(marker)){
+                                                  marker.info <- find.markerpos(cross, find.marker(cross, chr = chr, pos = pos))
+                                                  new.marker <- rownames(marker.info)
+                                                  new.pos <- marker.info$pos
+                                                }
+                                                return(c(new.marker,as.character(new.pos)))
+                                              }
+                                              
+                                              # Run single scan
+                                              non.parametric.scanone <-  scanone(x.non.parametric, pheno.col = i,  model = "np")
+                                              summary.non.parametric.scanone <- summary(non.parametric.scanone, threshold = LOD.THRESHOLD)
+                                              lod.count <- nrow(summary.non.parametric.scanone)
+                                              if(lod.count){
+                                                for(k in 1:lod.count){
+                                                  if(k > 1){
+                                                    #new.record <- record[0,] # Create an empty record object
+                                                    #new.record[1,] <- NA
+                                                    new.record <- record[1,] # Create copy of record object
+                                                    #new.record$ID <- NA # Drop the feature ID
+                                                  }else{
+                                                    new.record <- record # Copy record structured and data
+                                                  }
+                                                  #lod.count <- sum(non.parametric.scanone$lod >= LOD.THRESHOLD)
+                                                  
+                                                  #peak.lod <- non.parametric.scanone$lod == max(non.parametric.scanone$lod)
+                                                  # Extract Peak QTL information
+                                                  new.record$lg <- summary.non.parametric.scanone[k,"chr"]
+                                                  new.record$lod.peak <- summary.non.parametric.scanone[k,"lod"]
+                                                  new.record$pos.peak <- summary.non.parametric.scanone[k,"pos"]
+                                                  marker <- rownames(summary.non.parametric.scanone)[k]
+                                                  # Verify if current QTL has a pseudomarker
+                                                  marker.info <- transform.pseudomarker(x.non.parametric,marker,new.record$lg,new.record$pos.peak)
+                                                  new.record$marker <- marker.info[1]
+                                                  new.record$pos.peak <- as.numeric(marker.info[2])
+                                                  
+                                                  if(!is.na(new.record$lg)){
+                                                    new.record$qtl.ID <- with(new.record, sprintf("%s:%s@%f",features.np[i],lg,pos.peak))
+                                                  }
+                                                  
+                                                  p95.bayesian <- bayesint(non.parametric.scanone, chr = new.record$lg ,expandtomarkers = TRUE, prob = 0.95)
+                                                  p95.bayesian <- unique(p95.bayesian)
+                                                  #p95.bayesian <- summary(non.parametric.scanone,  perms=non.parametric.scanone.per, alpha=0.5, pvalues=TRUE)
+                                                  low.bound <- 1#p95.bayesian$pos == min(p95.bayesian$pos)
+                                                  upper.bound <- p95.bayesian$pos == max(p95.bayesian$pos)
+                                                  
+                                                  p95.bayesian$marker <- NA # Add new column for markers, prevent duplicated row names
+                                                  # Verify if the Bayesian interval QTLs have pseudomarkers
+                                                  for(l in 1:nrow(p95.bayesian)){
+                                                    marker <- rownames(p95.bayesian)[l]
+                                                    marker.info <- transform.pseudomarker(x.non.parametric,marker,p95.bayesian[l,"chr"],p95.bayesian[l,"pos"])
+                                                    p95.bayesian[l,"marker"] <- marker.info[1]
+                                                    p95.bayesian[l,"pos"] <- as.numeric(marker.info[2])
+                                                  }
+                                                  new.record$pos.p95.bay.int <- paste0(p95.bayesian[low.bound,"pos"],"-",
+                                                                                       p95.bayesian[upper.bound,"pos"])
+                                                  new.record$marker.p95.bay.int <- paste0(p95.bayesian[low.bound,"marker"],"-",
+                                                                                          p95.bayesian[upper.bound,"marker"])
+                                                  
+                                                  if(k > 1){
+                                                    record <- rbind(record,new.record)
+                                                  }else{
+                                                    record <- new.record
+                                                  }
+                                                }
+                                                
+                                                non.parametric.scanone.per <- scanone(x.non.parametric, pheno.col = i, model = "np", n.perm = PERMUTATIONS)
+                                                p5 <- summary(non.parametric.scanone.per)[[1]]  #  5% percent
+                                                p10 <- summary(non.parametric.scanone.per)[[2]] # 10% percent
+                                                
+                                                
+                                                lod.plot <- savePlot(plot(non.parametric.scanone, ylab="LOD Score") +
+                                                                       abline(h=p5, lwd=2, lty="solid", col="red") +
+                                                                       abline(h=p10, lwd=2, lty="solid", col="red"),
+                                                                     paste0(plots.directory,"/LOD-NP-",features.np[i]), width = 18)
+                                                
+                                                record[,]$p5.lod.thr <- p5
+                                                record[,]$p10.lod.thr <- p10
+                                                
+                                                p5.index <- record$lod.peak >= p5
+                                                p10.index <- record$lod.peak >= p10
+                                                if(!is.na(p5.index) && any(p5.index)){ record[p5.index,]$p5.qtl <- TRUE }
+                                                if(!is.na(p10.index)&& any(p10.index)){ record[p10.index,]$p10.qtl <- TRUE }
+                                                
+                                              }
+                                              record
+                                            }
+closeCluster(cl) # Stop cluster
 write.csv(x.normal.scanone, file = paste0(output.files.prefix,".normal.scanone.csv"))
-
-# initialize an Rmpi environment
-ns <- mpi.universe.size() - 1
-mpi.spawn.Rslaves(nslaves=ns)
-
-# In case R exits unexpectedly, have it automatically clean up
-# resources taken up by Rmpi (slaves, memory, etc...)
-.Last <- function(){
-  if (is.loaded("mpi_initialize")){
-    if (mpi.comm.size(1) > 0){
-      print("Please use mpi.close.Rslaves() to close slaves.")
-      mpi.close.Rslaves()
-    }
-    print("Please use mpi.quit() to quit R")
-    .Call("mpi_finalize")
-  }
-}
-
-# send these commands to the slaves
-mpi.bcast.cmd( id <- mpi.comm.rank() )
-mpi.bcast.cmd( ns <- mpi.comm.size() )
-mpi.bcast.cmd( host <- mpi.get.processor.name() )
-
-mpi.remote.exec(ls(.GlobalEnv))
-
-features <- features[-1] # Drop the ID column
-mpi.bcast.Robj2slave(features)
-bucket.size <- ceiling(length(features)/(mpi.comm.size()-1))
-mpi.bcast.Robj2slave(bucket.size)
-
-mpi.remote.exec(i <- (id - 1)*bucket.size + 1)
-mpi.remote.exec(f <- ifelse(id*bucket.size > length(test), length(test), id*bucket.size))
-
-summary.mapping <- function(i0,iN){
-  for(i in i0:iN){
-    transformation.info <- normal.transformed.meansp$feature == features[i]
-    transformation.info <- normal.transformed.meansp[transformation.info,c("transf","transf.value")][1,]
-    
-    record <- data.frame(
-      ID = i - 1,
-      qtl.ID = NA,
-      trait = features[i],
-      ind = individuals.phenotyped,
-      lg = NA,
-      lod.peak = NA,
-      pos.peak = NA,
-      marker = NA,
-      pos.p95.bay.int = NA,
-      marker.p95.bay.int = NA,
-      pvar = NA,
-      est.add = NA,
-      est.dom = NA,
-      p5.lod.thr = NA,
-      p10.lod.thr = NA,
-      p.val = NA,
-      transf = transformation.info$transf,
-      transf.val = transformation.info$transf.value,
-      method = "normal-scanone",
-      p5.qtl = FALSE,
-      p10.qtl = FALSE
-    )
-    
-    is.pseudo.marker <- function(marker){
-      if(grepl("loc",marker)){
-        return(TRUE)
-      }
-      return(FALSE)
-    }
-    
-    transform.pseudomarker <- function(cross, marker, chr, pos){
-      new.marker <- marker
-      new.pos <- pos
-      if(is.pseudo.marker(marker)){
-        marker.info <- find.markerpos(cross, find.marker(cross, chr = chr, pos = pos))
-        new.marker <- rownames(marker.info)
-        new.pos <- marker.info$pos
-      }
-      return(c(new.marker,as.character(new.pos)))
-    }
-    
-    # Run single scan
-    normal.scanone <-  scanone(x.normal, pheno.col = i,  model = "normal", method = "hk")
-    summary.normal.scanone <- summary(normal.scanone, threshold = LOD.THRESHOLD)
-    lod.count <- nrow(summary.normal.scanone)
-    if(lod.count){
-      for(k in 1:lod.count){
-        if(k > 1){
-          new.record <- record[1,] # Create copy of record object
-        }else{
-          new.record <- record # Copy record structured and data
-        }
-        # Extract Peak QTL information
-        new.record$lg <- summary.normal.scanone[k,"chr"]       
-        new.record$lod.peak <- summary.normal.scanone[k,"lod"]
-        new.record$pos.peak <- summary.normal.scanone[k,"pos"]
-        marker <- rownames(summary.normal.scanone)[k]
-        # Verify if current QTL has a pseudomarker
-        marker.info <- transform.pseudomarker(x.normal,marker,new.record$lg,new.record$pos.peak)
-        new.record$marker <- marker.info[1]
-        new.record$pos.peak <- as.numeric(marker.info[2])
-        
-        if(!is.na(new.record$lg)){
-          new.record$qtl.ID <- with(new.record, sprintf("%s:%s@%f",features[i],lg,pos.peak))
-        }
-        
-        p95.bayesian <- bayesint(normal.scanone, chr = new.record$lg ,expandtomarkers = TRUE, prob = 0.95)
-        p95.bayesian <- unique(p95.bayesian)
-        #p95.bayesian <- summary(normal.scanone,  perms=normal.scanone.per, alpha=0.5, pvalues=TRUE)
-        low.bound <- 1#p95.bayesian$pos == min(p95.bayesian$pos)
-        upper.bound <- p95.bayesian$pos == max(p95.bayesian$pos)
-        
-        p95.bayesian$marker <- NA # Add new column for markers, prevent duplicated row names
-        # Verify if the Bayesian interval QTLs have pseudomarkers
-        for(l in 1:nrow(p95.bayesian)){
-          marker <- rownames(p95.bayesian)[l]
-          marker.info <- transform.pseudomarker(x.normal,marker,p95.bayesian[l,"chr"],p95.bayesian[l,"pos"])
-          p95.bayesian[l,"marker"] <- marker.info[1]
-          p95.bayesian[l,"pos"] <- as.numeric(marker.info[2])
-        }
-        new.record$pos.p95.bay.int <- paste0(p95.bayesian[low.bound,"pos"],"-",
-                                             p95.bayesian[upper.bound,"pos"])
-        new.record$marker.p95.bay.int <- paste0(p95.bayesian[low.bound,"marker"],"-",
-                                                p95.bayesian[upper.bound,"marker"])
-        if(k > 1){
-          record <- rbind(record,new.record)
-        }else{
-          record <- new.record
-        }
-      }
-      
-      normal.scanone.per <- scanone(x.normal, pheno.col = i, model = "normal", method = "hk", n.perm = PERMUTATIONS)
-      p5 <- summary(normal.scanone.per)[[1]]  #  5% percent
-      p10 <- summary(normal.scanone.per)[[2]] # 10% percent
-      
-      
-      lod.plot <- savePlot(plot(normal.scanone, ylab="LOD Score") + 
-                             abline(h=p5, lwd=2, lty="solid", col="red") +
-                             abline(h=p10, lwd=2, lty="solid", col="red"),
-                           paste0(plots.directory,"/LOD-",features[i]), width = 18)
-      
-      record[,]$p5.lod.thr <- p5
-      record[,]$p10.lod.thr <- p10
-      
-      p5.index <- record$lod.peak >= p5
-      p10.index <- record$lod.peak >= p10
-      if(!is.na(p5.index) && any(p5.index)){ record[p5.index,]$p5.qtl <- TRUE }
-      if(!is.na(p10.index)&& any(p10.index)){ record[p10.index,]$p10.qtl <- TRUE }
-      
-      
-      chr <- as.numeric(summary.normal.scanone$chr)
-      pos <- as.numeric(summary.normal.scanone$pos)
-      qtl_s <- makeqtl(x.normal, chr, pos, what=c("prob"))
-      
-      for(m in 1:length(chr)){
-        f <- as.formula(paste0("y~",paste0("Q",m, collapse = " + ")))
-        fitqtl <- fitqtl(x.normal, pheno.col = i, qtl_s, formula = f , get.ests = TRUE, model = "normal", method="hk")
-        summary.fitqtl <- summary(fitqtl)
-        
-        if(length(summary.fitqtl)){
-          p.var <- as.numeric(summary.fitqtl[[1]][1,"%var"])
-          p.value.f <- as.numeric(summary.fitqtl[[1]][,"Pvalue(F)"])[1]
-          estimates <- as.numeric(summary.fitqtl$ests[,"est"])[-1]
-          record[m,]$pvar <- p.var
-          record[m,]$p.val <- p.value.f
-          record[m,]$est.add <- estimates[1]
-          record[m,]$est.dom <- estimates[2]
-        }
-      }
-    }
-  } 
-  #record
-}
+write.csv(x.normal.summary.mapping, file = paste0(output.files.prefix,".summary.mapping.csv"), row.names=FALSE, na="")
+write.csv(x.non.parametric.scanone, file = paste0(output.files.prefix,".non.parametric.scanone.csv"))
+write.csv(x.non.parametric.summary.mapping, file = paste0(output.files.prefix,".non.parametric.summary.mapping.csv"), row.names=FALSE, na="")
 
 print("Done with QTL Analysis")
+mpi.quit()
