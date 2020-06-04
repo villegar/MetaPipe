4fe8a1d88ecc530b2d82aae664cb0be72af83233
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-06-03 22:50:40 -0500

    Changing command to obtain current directory

--- get_openmpi.sh
+++ get_openmpi.sh
@@ -5,8 +5,8 @@ export VERSION=4.0.3
 if [ -f "openmpi/bin/mpirun"]; then
   echo "Using cached OpenMPI"
   echo "Expected: /home/travis/build/villegar/MetaPipe/openmpi-4.0.3"
-  export PATH=$PATH:$(pwd)/openmpi-$VERSION/bin
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/openmpi-$VERSION/lib
+  export PATH=$PATH:`pwd`/openmpi-$VERSION/bin
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/openmpi-$VERSION/lib
   echo "Found: $PATH"
 else
   echo "Downloading OpenMPI $VERSION source"
@@ -15,10 +15,11 @@ else
   rm openmpi-$VERSION.tar.gz
   echo "Configuring and building openmpi"
   cd openmpi-$VERSION
-  ./configure --prefix=$(pwd)
+  ./configure --prefix=`pwd`
   make -j 4 all
   make install
-  export PATH=$PATH:$(pwd)/bin
-  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/lib
+  export PATH=$PATH:`pwd`/bin
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/lib
+  echo "PATH: $PATH"
   cd ..
 fi
