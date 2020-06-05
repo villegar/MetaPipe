fdd7565c390f00bfa3c5b168a818bdb11f815264
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-06-04 20:23:50 -0500

    Adding unit test

--- tests/testthat/test-plots-util.R
+++ tests/testthat/test-plots-util.R
@@ -30,3 +30,15 @@ test_that("plot in TIFF format works", {
   file.remove(filename)
   expect_false(file.exists(filename))
 })
+
+test_that("compare histograms works", {
+  norm_dist <- rnorm(100)
+  norm_dist_transformed <- norm_dist^2
+  compare_hist(norm_dist, norm_dist_transformed, "XYZ", "compare_hist","")
+  filename <- "compare_hist_XYZ.png"
+  expect_true(file.exists(filename))
+  expect_false(dir.exists(filename))
+  expect_gt(file.size(filename),0)
+  file.remove(filename)
+  expect_false(file.exists(filename))
+})
