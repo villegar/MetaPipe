c5e200300eb5f97893283af027e9feee8a1ffe65
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-06-05 22:33:04 -0500

    Reformmating code

--- tests/testthat/test-plots-util.R
+++ tests/testthat/test-plots-util.R
@@ -1,10 +1,10 @@
 test_that("plot in PDF format works", {
-  savePlotPDF(hist(rnorm(100), main = "Histogram of Normal Distribution"), 
+  savePlotPDF(hist(rnorm(100), main = "Histogram of Normal Distribution"),
               "hist")
   filename <- "hist.pdf"
   expect_true(file.exists(filename))
   expect_false(dir.exists(filename))
-  expect_gt(file.size(filename),0)
+  expect_gt(file.size(filename), 0)
   file.remove(filename)
   expect_false(file.exists(filename))
 })
@@ -15,18 +15,18 @@ test_that("plot in PNG format works", {
   filename <- "hist.png"
   expect_true(file.exists(filename))
   expect_false(dir.exists(filename))
-  expect_gt(file.size(filename),0)
+  expect_gt(file.size(filename), 0)
   file.remove(filename)
   expect_false(file.exists(filename))
 })
 
 test_that("plot in TIFF format works", {
-  savePlotTIFF(hist(rnorm(100), main = "Histogram of Normal Distribution"), 
+  savePlotTIFF(hist(rnorm(100), main = "Histogram of Normal Distribution"),
                "hist")
   filename <- "hist.tiff"
   expect_true(file.exists(filename))
   expect_false(dir.exists(filename))
-  expect_gt(file.size(filename),0)
+  expect_gt(file.size(filename), 0)
   file.remove(filename)
   expect_false(file.exists(filename))
 })
@@ -38,7 +38,7 @@ test_that("compare histograms works", {
   filename <- "hist_XYZ.png"
   expect_true(file.exists(filename))
   expect_false(dir.exists(filename))
-  expect_gt(file.size(filename),0)
+  expect_gt(file.size(filename), 0)
   file.remove(filename)
   expect_false(file.exists(filename))
 })
@@ -49,7 +49,7 @@ test_that("generate histogram works", {
   filename <- "hist_XYZ.png"
   expect_true(file.exists(filename))
   expect_false(dir.exists(filename))
-  expect_gt(file.size(filename),0)
+  expect_gt(file.size(filename), 0)
   file.remove(filename)
   expect_false(file.exists(filename))
 })
