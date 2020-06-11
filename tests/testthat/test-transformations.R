e743e1c091733dd8ee12a7fb48b9f0e2d048d214
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-06-11 00:41:06 -0500

    Adding tests for power_transformation

--- tests/testthat/test-transformations.R
+++ tests/testthat/test-transformations.R
@@ -25,3 +25,24 @@ test_that("log transformation works", {
   file.remove(filename)
   expect_false(file.exists(filename))
 })
+
+test_that("power transformation works", {
+  set.seed(123)
+  data <- rnorm(100, 5)
+  expected_df <- data.frame(
+    flag = "Normal",
+    transf = "power",
+    transf.value = 2,
+    stringsAsFactors = FALSE
+  )
+  result_df <- power_transformation(sqrt(data), "SQRT_2")
+  expect_identical(expected_df, result_df)
+  expect_warning(result_df <- power_transformation(data, "SQRT_2"))
+  expect_null(result_df)
+  filename <- "HIST_POW_2_SQRT_2.png"
+  expect_true(file.exists(filename))
+  expect_false(dir.exists(filename))
+  expect_gt(file.size(filename), 0)
+  file.remove(filename)
+  expect_false(file.exists(filename))
+})
