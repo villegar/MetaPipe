a33a9ecc62da89e267507d2858a69625bcf53aa2
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-06-10 23:53:35 -0500

    Adding new tests for log_transformation

--- tests/testthat/test-transformations.R
+++ tests/testthat/test-transformations.R
@@ -4,3 +4,24 @@ test_that("check alpha level works", {
   expect_error(check_alpha(1.5), "alpha must be*")
   expect_error(check_alpha("1.5"), "alpha must be*")
 })
+
+test_that("log transformation works", {
+  set.seed(123)
+  data <- rnorm(100)
+  expected_df <- data.frame(
+    flag = "Normal",
+    transf = "log",
+    transf.value = 2,
+    stringsAsFactors = FALSE
+  )
+  result_df <- log_transformation(2^data, "POW_2")
+  expect_identical(expected_df, result_df)
+  expect_warning(result_df <- log_transformation(data, "POW_2"))
+  expect_null(result_df)
+  filename <- "HIST_LOG_2_POW_2.png"
+  expect_true(file.exists(filename))
+  expect_false(dir.exists(filename))
+  expect_gt(file.size(filename), 0)
+  file.remove(filename)
+  expect_false(file.exists(filename))
+})
