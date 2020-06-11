b23b0e68eb40cc6c59556b39ffa97bc57abff88f
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-06-11 12:46:18 -0500

    Including tests for root_transformation

--- tests/testthat/test-transformations.R
+++ tests/testthat/test-transformations.R
@@ -14,11 +14,11 @@ test_that("log transformation works", {
     transf.value = 2,
     stringsAsFactors = FALSE
   )
-  result_df <- log_transformation(2^data, "POW_2")
+  result_df <- log_transformation(2^data, "EXP_2")
   expect_identical(expected_df, result_df)
-  expect_warning(result_df <- log_transformation(data, "POW_2"))
+  expect_warning(result_df <- log_transformation(data, "EXP_2"))
   expect_null(result_df)
-  filename <- "HIST_LOG_2_POW_2.png"
+  filename <- "HIST_LOG_2_EXP_2.png"
   expect_true(file.exists(filename))
   expect_false(dir.exists(filename))
   expect_gt(file.size(filename), 0)
@@ -35,11 +35,32 @@ test_that("power transformation works", {
     transf.value = 2,
     stringsAsFactors = FALSE
   )
-  result_df <- power_transformation(sqrt(data), "SQRT_2")
+  result_df <- power_transformation(sqrt(data), "ROOT_2")
   expect_identical(expected_df, result_df)
-  expect_warning(result_df <- power_transformation(data, "SQRT_2"))
+  expect_warning(result_df <- power_transformation(data, "ROOT_2"))
   expect_null(result_df)
-  filename <- "HIST_POW_2_SQRT_2.png"
+  filename <- "HIST_POW_2_ROOT_2.png"
+  expect_true(file.exists(filename))
+  expect_false(dir.exists(filename))
+  expect_gt(file.size(filename), 0)
+  file.remove(filename)
+  expect_false(file.exists(filename))
+})
+
+test_that("root transformation works", {
+  set.seed(123)
+  data <- rnorm(100, 5)
+  expected_df <- data.frame(
+    flag = "Normal",
+    transf = "root",
+    transf.value = "e",
+    stringsAsFactors = FALSE
+  )
+  result_df <- root_transformation(data^2, "POW_2")
+  expect_identical(expected_df, result_df)
+  expect_warning(result_df <- root_transformation(data, "POW_2"))
+  expect_null(result_df)
+  filename <- "HIST_ROOT_e_POW_2.png"
   expect_true(file.exists(filename))
   expect_false(dir.exists(filename))
   expect_gt(file.size(filename), 0)
