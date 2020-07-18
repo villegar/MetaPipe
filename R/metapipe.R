59c6aa63d1ce81a9b2ffeae2f3f29b13e77da0cc
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-07-18 12:57:22 -0500

    Adding documentation and examples for the function replace_missing

--- R/metapipe.R
+++ R/metapipe.R
@@ -39,7 +39,31 @@ load_raw <- function(raw_data_filename, excluded_columns) {
   return(mean_raw_data)
 }
 
+#' Replace missing values (NAs)
+#'
+#' @param raw_data data frame containing the raw data
+#' @param excluded_columns vector containing the indices of the data set properties, excluded columns
+#' @param out_prefix prefix for output files, plots, etc.
+#' @param prop_na proportion of missing values, if a feature exceeds this threshold, then it is dropped out
+#' @param replace_na boolean flag to indicate whether or not missing values should be replaced by half of the minimum value
+#'
+#' @return data frame containing the raw data without missing values
+#' @export
+#'
+#' @examples
+#' #' example_data <- data.frame(ID = c(1,2,3,4,5), 
+#'                            P1 = c("one", "two", "three", "four", "five"), 
+#'                            F1 = rnorm(5), 
+#'                            F2 = rnorm(5))
+#' example_data$F1[2:3] <- NA 
+#' example_data$F2[4] <- NA 
+#' replace_missing(example_data, c(2))
+#' replace_missing(example_data, c(2), prop_na =  0.25)
+#' replace_missing(example_data, c(2), replace_na =  TRUE)
 replace_missing <- function(raw_data, excluded_columns, out_prefix = getwd(), prop_na = 0.5, replace_na = FALSE) {
+  # Exclude column 1, ID
+  excluded_columns <- unique(c(1, excluded_columns))
+  
   # Missing values are replaced by half of the minimum non-zero value for each feature.
   if(replace_na) {
     NA2halfmin <- function(x) suppressWarnings(replace(x, is.na(x), (min(x, na.rm = TRUE)/2)))
@@ -48,8 +72,8 @@ replace_missing <- function(raw_data, excluded_columns, out_prefix = getwd(), pr
     NACount <- which(colMeans(is.na(raw_data[,-excluded_columns])) >= prop_na) + length(excluded_columns)
     if(length(NACount)) {
       write.csv(raw_data[, c(excluded_columns, NACount)], file = paste0(out_prefix,"_NA_raw_data.csv"), row.names = FALSE)
-      cat(paste0("The following features were dropped because they have ", (prop_na*100), "% or more missing values:\n"))
-      cat(colnames(raw_data)[NACount])
+      cat(paste0("The following features were dropped because they have ", (prop_na*100), "% or more missing values: "))
+      cat(paste0(paste(colnames(raw_data)[NACount], collapse = ", "), "\n"))
       raw_data[, NACount] <- NULL
     }
   }
