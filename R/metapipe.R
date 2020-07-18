f055d2154570c9c3be5ab8efb9cf8e82285074e6
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-07-18 11:30:24 -0500

    Minor changes in object names and adding comments

--- R/metapipe.R
+++ R/metapipe.R
@@ -1,17 +1,23 @@
 
 load_raw <- function(raw_data_filename, excluded_columns) {
   # Load and clean raw data
-  raw_data <- read.csv(raw_data) 
+  raw_data <- read.csv(raw_data_filename) 
   ncols <- ncol(raw_data)
   len_excluded_columns <- length(excluded_columns)
+  
+  # Aggregate data by row, computing the mean
   mean_raw_data <- aggregate(raw_data[, (len_excluded_columns + 1):ncols],
                              by = list(raw_data[, 1]),
                              mean, 
                              na.action = na.omit)
   colnames(mean_raw_data)[1] <- colnames(raw_data)[1]
+  
+  # Joins the agreggated data with the excluded columns (properties)
   mean_raw_data <- left_join(raw_data[, excluded_columns],
                              mean_raw_data,
                              by = colnames(raw_data)[1])
+  
+  # Find duplicated row values on the first column (ID)
   mean_raw_data <- mean_raw_data[!duplicated(mean_raw_data[, 1]),]
   rownames(mean_raw_data) <- 1:nrow(mean_raw_data)
   mean_raw_data_rows <- nrow(mean_raw_data)
