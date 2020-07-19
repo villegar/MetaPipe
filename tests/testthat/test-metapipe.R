a3f6a0336e31ff762fa33110baa37b238711d4c3
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-07-18 19:13:32 -0500

    Adding tests for replace_missing function

--- tests/testthat/test-metapipe.R
+++ tests/testthat/test-metapipe.R
@@ -32,3 +32,39 @@ test_that("load raw data works", {
   expect_false(file.exists(filename))
   expect_false(file.exists(filename_dup))
 })
+
+test_that("replace missing data works", {
+  set.seed(123)
+  # Data frame with the raw data: 1 property and 2 features
+  example_data <- data.frame(ID = c(1,2,3,4,5), 
+                             P1 = c("one", "two", "three", "four", "five"), 
+                             F1 = rnorm(5), 
+                             F2 = rnorm(5))
+ 
+  # Inserting missing values manually
+  example_data$F1[2:3] <- NA
+  example_data$F2[4] <- NA
+  
+  # Expected output when replacing by half of the minimum value
+  example_data_alt <- example_data[,]
+  example_data_alt$F1[2:3] <- min(example_data$F1, na.rm = TRUE)/2
+  example_data_alt$F2[4] <- min(example_data$F2, na.rm = TRUE)/2
+  
+  # Testing the function with different parameters
+  results_1 <- replace_missing(example_data, c(2))
+  expect_message(results_2 <- replace_missing(example_data, c(2), prop_na =  0.25))
+  results_3 <- replace_missing(example_data, c(2), replace_na =  TRUE)
+  
+  # Comparing results
+  expect_equal(example_data, results_1)
+  expect_equal(example_data[, colnames(example_data) != "F1"], results_2)
+  expect_equal(example_data_alt, results_3)
+  
+  # Checking for file generated in the test where prop_na =  0.25
+  filename <- "metapipe_NA_raw_data.csv"
+  expect_true(file.exists(filename))
+  expect_false(dir.exists(filename))
+  expect_gt(file.size(filename), 0)
+  file.remove(filename)
+  expect_false(file.exists(filename))
+})
