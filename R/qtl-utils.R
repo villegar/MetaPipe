90eb7c874f388c333d00f5e244fc222307934216
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-08-10 18:33:10 -0500

    Adding examples for transform_pseudo_marker

--- R/qtl-utils.R
+++ R/qtl-utils.R
@@ -23,7 +23,37 @@ is_pseudo_marker <- function(marker) {
 #' @return a prime marker and position
 #' @export
 #'
-# @examples
+#' @examples
+#' # Create toy dataset
+#' excluded_columns <- c(2)
+#' population <- 5
+#' seed <- 1
+#' example_data <- data.frame(ID = 1:population,
+#'                            P1 = c("one", "two", "three", "four", "five"),
+#'                            F1 = rnorm(population),
+#'                            F2 = rnorm(population))
+#' example_data_normalised <- data.frame(index = rep(c(1, 2), each = 5),
+#'                                       feature = rep(c("F1", "F2"), each = 5),
+#'                                       values = c(example_data$F1, example_data$F2),
+#'                                       flag = "Normal",
+#'                                       transf = "",
+#'                                       transf_val = NA,
+#'                                       stringsAsFactors = FALSE)
+#' assess_normality_postprocessing(example_data, excluded_columns, example_data_normalised, out_prefix = here::here("metapipe"))
+#' 
+#' # Create and store random genetic map [for testing only]
+#' genetic_map <- random_map(population = population, seed = seed)
+#' write.csv(genetic_map, here::here("metapipe_genetic_map.csv"), row.names = FALSE)
+#' print(list.files(here::here()))
+#' # Load cross file with genetic map and raw data for normal features
+#' x <- qtl::read.cross(format = "csvs", 
+#'                      dir = here::here(),
+#'                      genfile = "metapipe_genetic_map.csv",
+#'                      phefile = "metapipe_raw_data_norm.csv")
+#' set.seed(seed)
+#' x <- qtl::jittermap(x)
+#' x <- qtl::calc.genoprob(x, step = 1, error.prob = 0.001)
+#' transform_pseudo_marker(x, 'loc1', 1, 2.0)
 transform_pseudo_marker <- function(x_data, marker, chr, pos) {
   markerp <- marker
   posp <- pos
