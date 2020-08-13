c6e69e5274a432d6d180eda3bd4802c6ba3f4ba2
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-08-13 13:14:59 -0500

    Adding foreach library call

--- R/metapipe.R
+++ R/metapipe.R
@@ -497,7 +497,7 @@ qtl_perm_test <- function(x_data, cpus = 1, qt_method = "scanone", raw_data_norm
   # Obtain number of individuals (population)
   num_indv <- summary(x_data)[[2]]
   
-  x_sum_map <- foreach(i = feature_indices,
+  x_sum_map <- foreach::foreach(i = feature_indices,
                             .combine = rbind) %dopar% {
                               if (!is.null(raw_data_normalised)) {
                                 transf_info <- raw_data_normalised$feature == features[i]
