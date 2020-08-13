f9e7272ecf77e67ef307ead4888a5786720920ba
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-08-13 15:43:13 -0500

    Adding test for qtl_perm_test

--- tests/testthat/test-metapipe.R
+++ tests/testthat/test-metapipe.R
@@ -308,3 +308,60 @@ test_that("qtl mapping scanone works", {
     expect_false(file.exists(f))
   }
 })
+
+test_that("qtl mapping permutation test with scanone works", {
+  # Create toy dataset
+  excluded_columns <- c(2)
+  population <- 5
+  seed <- 123
+  set.seed(seed)
+  setwd(here::here())
+  example_data <- data.frame(ID = 1:population,
+                             P1 = c("one", "two", "three", "four", "five"),
+                             F1 = rnorm(population),
+                             F2 = rnorm(population))
+  example_data_normalised <- data.frame(index = rep(c(1, 2), each = 5),
+                                        feature = rep(c("F1", "F2"), each = 5),
+                                        values = c(example_data$F1, example_data$F2),
+                                        flag = "Normal",
+                                        transf = "",
+                                        transf_val = NA,
+                                        stringsAsFactors = FALSE)
+  assess_normality_postprocessing(example_data, excluded_columns, example_data_normalised)
+  
+  # Create and store random genetic map [for testing only]
+  genetic_map <- random_map(population = population, seed = seed)
+  write.csv(genetic_map, "metapipe_genetic_map.csv", row.names = FALSE)
+  expect_true(file.exists("metapipe_genetic_map.csv"))
+  
+  x <- qtl::read.cross("csvs", here::here(),
+                       genfile = "metapipe_genetic_map.csv",
+                       phefile = "metapipe_raw_data_norm.csv")
+  features <- colnames(x$pheno)
+  set.seed(seed)
+  x <- qtl::jittermap(x)
+  x <- qtl::calc.genoprob(x, step = 1, error.prob = 0.001)
+  x_qtl_perm <- MetaPipe::qtl_perm_test(x, n_perm = 5, model = "normal", method = "hk")
+  expect_equal(c(5, 20), dim(x_qtl_perm))
+  
+  filenames <- c("LOD-F1.png", 
+                 "LOD-F2.png")
+  for (f in filenames) {
+    expect_true(file.exists(f))
+    expect_false(dir.exists(f))
+    expect_gt(file.size(f), 0)
+    file.remove(f)
+    expect_false(file.exists(f))
+  }
+  
+  # Delete temporary files
+  filenames <- c("metapipe_normalisation_stats.csv", 
+                 "metapipe_raw_data_non_par.csv", 
+                 "metapipe_raw_data_norm.csv", 
+                 "metapipe_raw_data_normalised_all.csv", 
+                 "metapipe_genetic_map.csv")
+  for (f in filenames) {
+    file.remove(f)
+    expect_false(file.exists(f))
+  }
+})
