f30e95a29ae98c0880583a439ad25c9430603b17
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-08-14 20:47:18 -0500

    Adding URL to logo and modifying example

--- R/plots-util.R
+++ R/plots-util.R
@@ -192,7 +192,7 @@ generate_hist <- function(data, feature, prefix, xlab){
 #' @export
 #'
 #' @examples
-#' hex_logo()
+#' hex_logo(output = "inst/images/metapipe.png")
 hex_logo <- function(subplot = system.file("images/lab-2.png", package = "MetaPipe"),
                      dpi = 800,
                      h_color = "#000000",
@@ -205,5 +205,7 @@ hex_logo <- function(subplot = system.file("images/lab-2.png", package = "MetaPi
                       # l_x = 1.0, l_y = 1.0, spotlight = FALSE, 
                       s_x = 1.0, s_y = .85, s_width = .5,
                       p_x = 1.0, p_y = 1.52, p_size = 6, p_color = p_color,
+                      url = "https://github.com/villegar/MetaPipe", 
+                      u_angle = 30, u_color = p_color, u_size = 1.35,
                       filename = output)
 }
