d05b579d70f96c4908a29c21e0dd11eb1b031f72
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-10-10 14:04:08 +0100

    Adding tests for check_types

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,10 @@
+test_that("check data types works", {
+  example_data <- data.frame(ID = c(1,2,3,4,5), 
+                             P1 = c("one", "two", "three", "four", "five"), 
+                             T1 = rnorm(5))
+  expect_equal(check_types(example_data), 2)
+  expect_equal(check_types(example_data, numeric = FALSE), c(1, 3))
+  expect_equal(check_types(example_data, excluded_columns = 1, numeric = FALSE),
+               c(1, 2))
+  expect_warning(check_types(example_data, quiet = FALSE))
+})
