44bbfae21af53c0abace72ef8c3281708a56bdef
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-10-10 23:02:12 +0100

    Removing outliers

--- data-raw/ionomics.R
+++ data-raw/ionomics.R
@@ -4,4 +4,14 @@ ionomics_path <- system.file("extdata",
                              package = "MetaPipe", 
                              mustWork = TRUE)
 ionomics <- MetaPipe::load_raw(ionomics_path)
+ionomics <- ionomics[order(ionomics$ID), ]
+rownames(ionomics) <- seq_len(nrow(ionomics))
+# This were inspected manually
+ionomics[ionomics[, 6] > 0.10, 6] <- NA
+ionomics[ionomics[, 8] > 50, 8] <- NA
+ionomics[ionomics[, 10] > 40, 10] <- NA
+ionomics[ionomics[, 12] > 100, 12] <- NA
+ionomics[ionomics[, 14] > 0.4, 14] <- NA
+ionomics[ionomics[, 15] > 1.5, 15] <- NA
+ionomics[abs(ionomics[19]) > 1.5, 19] <- NA
 usethis::use_data(ionomics, overwrite = TRUE)
