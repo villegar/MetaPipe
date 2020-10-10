b7f8746b59acd56aa4fa395019b4e85a229467f0
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-10-10 10:38:40 +0100

    Adding ionomics data

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' F1 Seedling Ionomics dataset
+#' 
+#' A dataset containing information of 21 different isotopes and sample weights  
+#' of 218 samples.
+#' 
+#' @format A data frame with 218 rows and 23 variables:
+#' \describe{
+#'     \item{ID}{Sample ID}
+#'     \item{SampleWeight}{Sample weight, in X}
+#'     \item{Ca44}{Calcium 44 isotope, in atom \%}
+#'     \item{K39}{Potassium 39 isotope, in atom \%}
+#'     \item{P31}{Phosphorus 31 isotope, in atom \%}
+#'     \item{Li7}{Lithium 7 isotope, in atom \%}
+#'     \item{B11}{Boron 11 isotope, in atom \%}
+#'     \item{Na23}{Sodium 23, in atom \%}
+#'     \item{Mg26}{Magnesium 26, in atom \%}
+#'     \item{Al27}{Aluminium 27, in atom \%}
+#'     \item{S34}{Sulfur 34, in atom \%}
+#'     \item{Fe54}{Iron 54, in atom \%}
+#'     \item{Mn55}{Manganese 55, in atom \%}
+#'     \item{Co59}{Cobalt 59, in atom \%}
+#'     \item{Ni60}{Nickel 60, in atom \%}
+#'     \item{Cu63}{Copper 63, in atom \%}
+#'     \item{Zn66}{Zinc 66, in atom \%}
+#'     \item{As75}{Arsenic 75, in atom \%}
+#'     \item{Se78}{Selenium 78, in atom \%}
+#'     \item{Rb85}{Rubidium 85, in atom \%}
+#'     \item{Sr88}{Strontium 88, in atom \%}
+#'     \item{Mo98}{Molybdenum 98, in atom \%}
+#'     \item{Cd111}{Cadmium 111, in atom \%}
+#' }
+#' @author Jason P. Londo \email{jason.londo@usda.gov}
+#' @usage data(ionomics)
+#' @source \url{https://fennell-lab.netlify.app}
+"ionomics"
